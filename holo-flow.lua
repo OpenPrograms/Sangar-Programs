4d550614ec660d0e2375f466e625b36a9980addb
Vexatos <Vexatos@users.noreply.github.com>
2014-06-20 16:56:38 +0200

    Update holo-flow.lua

--- holo-flow.lua
+++ holo-flow.lua
@@ -4,6 +4,8 @@
 local component = require("component")
 local noise = require("noise")
 local holo = component.hologram
+local keyboard = require("keyboard")
+print("Press Ctrl+W to stop")
 
 local i = 0
 while true do
@@ -11,7 +13,10 @@ while true do
   i = i + 0.05
   for x = 1, 16 * 3 do
     for z = 1, 16 * 3 do
-      holo.fill(x, z, 15 + noise.fbm(x/(16*3) + i, 1, z/(16*3) + i) * 28)
+      holo.fill(x, z, 15 + noise.fbm(x/(16*3) + i, 1, z/(16*3) + i) * 28,1)
+      if keyboard.isKeyDown(keyboard.keys.w) and keyboard.isControlDown() then
+        os.exit()
+      end
     end
   end
 end
