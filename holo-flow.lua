db31129d5b72774e0e5ad503aaed8d19bc58222e
Florian Nücke <florian@nuecke.de>
2015-04-19 11:44:13 +0200

    Fixed holo-flow.lua not clearing.

--- holo-flow.lua
+++ holo-flow.lua
@@ -14,7 +14,9 @@ while true do
   i = i + 0.05
   for x = 1, 16 * 3 do
     for z = 1, 16 * 3 do
-      holo.fill(x, z, 15 + noise.fbm(x/(16*3) + i, 1, z/(16*3) + i) * 28,1)
+      local yMax = 15 + noise.fbm(x/(16*3) + i, 1, z/(16*3) + i) * 28
+      holo.fill(x, z, yMax, 1)
+      holo.fill(x, z, yMax + 1, 32, 0)
       if keyboard.isKeyDown(keyboard.keys.w) and keyboard.isControlDown() then
         os.exit()
       end
