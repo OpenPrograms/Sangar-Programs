56f4bca41853e7d0d6136a74cd62e76c3aa1fc64
Florian Nücke <florian@nuecke.de>
2015-07-31 21:39:51 +0200

    Made the parameter parsing in geo2holo more compact.

--- geo2holo.lua
+++ geo2holo.lua
@@ -1,46 +1,30 @@
 local component = require("component")
 if not component.isAvailable("geolyzer") then
-  io.stderr:write("This program requires a Geolyzer to run.")
+  io.stderr:write("This program requires a Geolyzer to run.\n")
   return
 end
 if not component.isAvailable("hologram") then
-  io.stderr:write("This program requires a Hologram Projector to run.")
+  io.stderr:write("This program requires a Hologram Projector to run.\n")
   return
 end
 
 local sx, sz = 48, 48
 local ox, oz = -24, -24
-local starty, stopy = -5, 27
+local starty, stopy = -5
+
+local function validateY(value, min, max, default)
+  value = tonumber(value) or default
+  if value < min or value > max then
+    io.stderr:write("invalid y coordinate, must be in [" .. min .. ", " .. max .. "]\n")
+    os.exit(1)
+  end
+  return value
+end
 
 do
   local args = {...}
-  if #args >= 1 then
-    local newY = tonumber(args[1])
-    if newY then
-      if newY >= -32 and newY <= 31 then
-        starty = newY
-        stopy = math.min(starty + 31, stopy)
-      else
-        io.strerr:write("Minimum Y coordinate must be between -32 and 31.")
-        return
-      end
-    end
-    if #args >= 2 then
-      local newY = tonumber(args[2])
-      if newY then
-        if newY >= starty and newY <= 31 then
-          if newY - starty <= 32 then
-            stopy = newY
-          else
-            io.stderr:write("Minimum and maximum Y coordinates must be 32 or less apart.")
-          end
-        else
-          io.stderr:write("Maximum Y coordinate must be between -32 and 31 and larger than or equal to the minimum.")
-          return
-        end
-      end
-    end
-  end
+  starty = validateY(args[1], -32, 31, starty)
+  stopy = validateY(args[2], starty, starty + 32, math.min(starty + 32, 31))
 end
 
 component.hologram.clear()
@@ -48,11 +32,10 @@ for x=ox,sx+ox do
   for z=oz,sz+oz do
     local hx, hz = 1 + x - ox, 1 + z - oz
     local column = component.geolyzer.scan(x, z, false)
-    local hy = 1
-    for y=starty + 1,stopy + 1 do
+    for y=1,1+stopy-starty do
       local color = 0
       if column then
-        local hardness = column[y + 32]
+        local hardness = column[y + starty + 32]
         if hardness == 0 or not hardness then
           color = 0
         elseif hardness < 3 then
@@ -64,11 +47,10 @@ for x=ox,sx+ox do
         end
       end
       if component.hologram.maxDepth() > 1 then
-        component.hologram.set(hx, hy, hz, color)
+        component.hologram.set(hx, y, hz, color)
       else
-        component.hologram.set(hx, hy, hz, math.min(color, 1))
+        component.hologram.set(hx, y, hz, math.min(color, 1))
       end
-      hy = hy + 1
     end
     os.sleep(0)
   end
